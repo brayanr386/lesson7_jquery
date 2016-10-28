9f07f7435a6a0b528cf4dc8c2c6e588daa54c975
Brayan Rodriguez <brayanr386@yahoo.com>
2016-10-28 08:47:26 +0000

    lesson 7 final commit

---
+++ Lesson7_Assignment/script.js
@@ -0,0 +1,15 @@
+/*
+Project Name:  Recipe Display Application
+      
+      Author: Brayan Rodriguez
+      Date:   10/28/16
+      Filename: script.js
+*/
+
+//displays the next element after the current target 
+function display(event) {
+    $(event.currentTarget) .next() .fadeIn("slow");
+}//end of display
+
+//attach event listener to h3 elements to invoke display function when clicked
+$("h3") .click(display);
